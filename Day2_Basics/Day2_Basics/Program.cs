5e9900544779e8effe20de9f822456abf266c1ae
findbtw <maksimosipenko7@gmail.com>
2026-07-27 18:16:31 +0200

    Day 2: conditionals - age classifier and grade classifier

---
+++ Day2_Basics/Day2_Basics/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Ваш бал за экзамен: ");
+
+int grade = Convert.ToInt32(Console.ReadLine());
+
+string result = grade switch
+{
+    >= 90 => "A",
+    >= 75 => "B",
+    >= 60 => "C",
+    >= 40 => "D",
+    _ => "F"
+};
+Console.WriteLine($"Ваша оценка: {result}");
