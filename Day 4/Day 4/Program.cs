51ec0266857950f0279242178f32010e19f4ff15
findbtw <maksimosipenko7@gmail.com>
2026-07-29 20:30:12 +0200

    Day 4: arrays, List<T>, and 2D arrays - traversal and accumulator patterns

--- Day 4/Day 4/Program.cs
+++ Day 4/Day 4/Program.cs
@@ -1,26 +1,11 @@
-﻿List<int> array = new List<int> { 1, 2, 3, 4, 5, 10, 12, 23, 44, 55 };
-array.Add(13);
-array.Add(22);
-array.Add(67);
-array.Remove(10);
-int sum = 0;
-int max = array[0];
-int min = array[0];
-double avarage;
-for (int i = 0; i < array.Count; i++)
+﻿int[,] array = new int[3, 3];
+Random rand = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    sum += array[i];
-    if (array[i] > max)
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        max = array[i];
+        array[i, j] = rand.Next(1, 10);
+        Console.Write(array[i,j] + " ");
     }
-    if (array[i] < min)
-    {
-        min = array[i];
-    }
+    Console.WriteLine();
 }
-avarage = (double)sum / array.Count;
-Console.WriteLine($"Sum: {sum}");
-Console.WriteLine($"Max: {max}");
-Console.WriteLine($"Min: {min}");
-Console.WriteLine($"Average: {avarage}");
