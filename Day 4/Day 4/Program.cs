155f351eb37ff8ebf857d68eaec6e45d7a8e2047
findbtw <maksimosipenko7@gmail.com>
2026-07-29 20:08:33 +0200

    Day 4: arrays - manual sum/avg/min/max without LINQ

---
+++ Day 4/Day 4/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] array  = new int[10] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int num = 0;
+double average = 0;
+int max = array[0];
+int min = array[0];
+for  (int i = 0; i < array.Length; i++)
+{
+        num += array[i];
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+Console.WriteLine("Sum: " + num);
+average = (double)num / array.Length;
+Console.WriteLine("Average: " + average);
+Console.WriteLine("Max: " + max);
+Console.WriteLine("Min: " + min);
+
